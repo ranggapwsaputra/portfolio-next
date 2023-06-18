3b7031bfe678dc7f4f925c26332b1335f5c0359f
Rangga Saputra <131574835+ranggapwsaputra@users.noreply.github.com>
2023-06-19 02:34:08 +0700

    push transparency

--- config/navigation.config.ts
+++ config/navigation.config.ts
@@ -24,7 +24,7 @@ export const headerNavigation: NavigationItem[] = [
 
 export const footerNavigation: NavigationItem[] = [
   {
-    to: '/transparency.ts',
+    to: '/transparency',
     label: 'Transparency',
   }
 ];
